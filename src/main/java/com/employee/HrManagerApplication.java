f45c0902f14a2237eedd6a68a350320ade02db56
priteshsingh-vats <42402753+priteshsingh-vats@users.noreply.github.com>
2021-09-23 14:20:39 +0530

    Update HrManagerApplication.java

--- src/main/java/com/employee/HrManagerApplication.java
+++ src/main/java/com/employee/HrManagerApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class HrManagerApplication {
